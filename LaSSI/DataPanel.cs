d1142705f6ffbdffceac51e50ebdb8a0d47340ff
Ben <ben@telnetx.net>
2024-01-07 23:56:37 +0000

    Don't change foreground color by default, modify color for NY

--- LaSSI/DataPanel.cs
+++ LaSSI/DataPanel.cs
@@ -1534,7 +1534,6 @@ namespace LaSSI
          {
             if (mainForm is not null)
             {
-               e.ForegroundColor = Colors.White;
                if (mainForm.prefs.holidayFun.value is not null and yesno holidayfun && holidayfun == yesno.yes)
                {
                   var today = DateTime.Today;
@@ -1553,7 +1552,7 @@ namespace LaSSI
                   }
                   else if (today >= NewYearDay && today < NewYearDay.AddDays(3))
                   {
-                     e.ForegroundColor = e.Row % 2 == 0 ? Colors.Cyan : Colors.Pink;
+                     e.ForegroundColor = Colors.HotPink;
                   }
                }
             }
