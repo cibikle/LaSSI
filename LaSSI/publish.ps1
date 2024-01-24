d903d2da1098a705f8c9e87bc1dfa93b9c4fdcfa
cibikle <cibikle@gmail.com>
2024-01-23 20:29:20 -0500

    Update publish.ps1
    Removed unneeded Mac steps, cleaned up Windows steps, added Linux steps

--- LaSSI/publish.ps1
+++ LaSSI/publish.ps1
@@ -4,38 +4,25 @@ param (
 )
 
 $directory = $PSScriptRoot;
-#$prevDirectory = (Get-Location).Path;
 
 $version = "0.$version";
 
 try {
-    #Set-Location $directory;
     pushd $directory
     dotnet publish -c Release --self-contained -r win-x64 -p:PublishSingleFile=true
-    #dotnet publish -c Release --self-contained -r linux-x64 -p:PublishSingleFile=true
-    #dotnet publish -c Release --self-contained -r osx-x64 -p:PublishSingleFile=true
-    #dotnet build -c Release LaSSI.csproj -p:BuildPlatform=Mac64 -p:MacBundleDotNet=true
+    dotnet publish -c Release --self-contained -r linux-x64 -p:PublishSingleFile=true
 
     $builtFilePath = "bin/Wpf/Release/net6.0-windows/{0}/publish/";
-    $compressedFilePath = "bin/Wpf/Release/net6.0-windows/{0}/LaSSI.{0}.v$version.zip";
-    #$builtFilePathMac = "bin/Mac64/Release/net6.0/osx-x64/"
+    $builtFilePathLinux = "bin/Gtk/Release/net6.0/linux-x64/publish/"
     
     $win64Spec = "win-x64";
-    #$linux64Spec = "linux-x64";
-    #$macx64Spec = "osx-x64";
-    #Compress-Archive -Path ($builtFilePath -f $linux64Spec) -DestinationPath ($compressedFilePath -f $linux64Spec)
-    #Compress-Archive -Path ($builtFilePath -f $macx64Spec) -DestinationPath ($compressedFilePath -f $macx64Spec)
 
     New-Item "output" -ItemType Directory;
     
-    #Compress-Archive -Path ($builtFilePath -f $win64Spec) -DestinationPath (Join-Path -Path "output" -ChildPath ($compressedFilePath -f $win64Spec))
     Move-Item -Path ($builtFilePath -f $win64Spec) -Destination "output";
-    #Move-Item -Path $builtFilePathMac -Destination "output";
-    #Move-Item -Path ($compressedFilePath -f $linux64Spec) -Destination "output";
-    #Move-Item -Path ($compressedFilePath -f $macx64Spec) -Destination "output";
+    Move-Item -Path $builtFilePathLinux -Destination "output";
     ls output
 }
 finally {
-    #Set-Location $prevDirectory;
     popd
 }
