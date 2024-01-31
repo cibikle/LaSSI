10448489136ef39355e1bf55049c099e794249d9
Christian Bikle <cibikle@gmail.com>
2024-01-30 21:03:15 -0500

    designer test

---
+++ LaSSI/MyPanel1.xeto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Eto.Forms;
+using Eto.Drawing;
+using Eto.Serialization.Xaml;
+
+namespace LaSSI
+{	
+	public class MyPanel1 : Dialog
+	{	
+		public MyPanel1()
+		{
+			XamlReader.Load(this);
+		}
+	}
+}
